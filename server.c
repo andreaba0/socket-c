bf3dd6b5f65eb851745fe47054e25193581cb8cc
andreaba0 <andreabarchietto01@gmail.com>
2022-05-27 13:45:02 +0200

    backup

--- server.c
+++ server.c
@@ -4,12 +4,25 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
 
-int main(int argc, char*argv[])
+int main(int argc, char *argv[])
 {
-    int socket_desc, client_sock, client_size;
+    srand(time(NULL));
+    char *parole[6] = {"etere", "fessa", "situa", "razzi", "mezzo", "tende"};
+    unsigned int socket_desc, client_sock, client_size;
     struct sockaddr_in server_addr, client_addr;
-    char server_message[2000], client_message[2000];
+    char server_message[256], client_message[256];
+    int maxTentativi;
+    if (argc < 2 || argc > 3)
+    {
+        printf("./server.out <porta> <tentativi>\n");
+        return 1;
+    }
+    if (argc < 3)
+        maxTentativi = 6;
+    else
+        maxTentativi = atoi(argv[2]);
 
     // Clean buffers:
     memset(server_message, '\0', sizeof(server_message));
@@ -57,17 +70,60 @@ int main(int argc, char*argv[])
     }
     printf("Client connected at IP: %s and port: %i\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
-    while(1) {
+    memset(server_message, '\0', sizeof(server_message));
+    sprintf(server_message, "OK %s Inizio del gioco\n", argv[2]);
+    if (send(client_sock, server_message, strlen(server_message), 0) < 0)
+    {
+        printf("Errore nell'invio del messaggio\n");
+    }
+    int parola = rand() % 6;
+
+    for (int i = 0; i < maxTentativi + 1; i++)
+    {
         memset(client_message, '\0', sizeof(client_message));
-        if(recv(client_sock, client_message, sizeof(client_message), 0) < 0) {
-            printf("errro\n");
-        } else {
-            printf("Client: %s\n", client_message);
+        memset(server_message, '\0', sizeof(server_message));
+        if (i == maxTentativi)
+        {
+            sprintf(server_message, "END %d %s\n", maxTentativi, parole[parola]);
+            send(client_sock, server_message, strlen(server_message), 0);
+            break;
         }
-        if(strcmp(client_message, "QUIT")==0) break; 
-        if(send(client_sock, client_message, strlen(client_message), 0)<0) {
-            printf("Unable to send\n");
+        recv(client_sock, client_message, sizeof(client_message), 0);
+        char *tempString = strdup(client_message);
+        char *token = strtok(tempString, " ");
+        if(token==NULL) {
+            sprintf(server_message, "ERR Richiesto un comando\n");
+            send(client_sock, server_message, strlen(server_message), 0);
         }
+        if (strcmp(tempString, "QUIT") == 0)
+        {
+            sprintf(server_message, "QUIT Alla prossima!\n");
+            send(client_sock, server_message, strlen(server_message), 0);
+            break;
+        }
+        if (strcmp(tempString, "WORD") != 0)
+        {
+            sprintf(server_message, "ERR Comando '%s' sconosciuto\n", tempString);
+            send(client_sock, server_message, strlen(server_message), 0);
+        }
+        token = strtok(NULL, " ");
+        if(token==NULL) {
+            sprintf(server_message, "ERR Richiesta una parola\n");
+            send(client_sock, server_message, strlen(server_message), 0);
+        }
+        if (strlen(tempString)!=5)
+        {
+            sprintf(server_message, "ERR la parola deve essere di 5 caratteri\n");
+            send(client_sock, server_message, strlen(server_message), 0);
+            break;
+        }
+        if(strcmp(tempString, parole[parola])==0) {
+            sprintf(server_message, "OK PERFECT\n");
+            send(client_sock, server_message, strlen(server_message), 0);
+            break;
+        }
+        sprintf(server_message, "OK %d _____\n", i);
+        send(client_sock, server_message, strlen(server_message), 0);
     }
 
     // Closing the socket:
